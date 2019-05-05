295ab753c3c2a9dcc7f65c69ea79a85c6566ce5c
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-05-05 14:00:54 +0700

    Add files via upload

---
+++ editor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "struktur.h"
+
+using namespace std;
+
+int main(){
+print "[Simple Text Editor By Fajar Firdaus]\n";
+cout << "––––––––––––––––––––––––––––––––––––\n";
+teks nama_file;
+print "Masukan Nama file : ";
+masuk nama_file;
+
+ofstream f;
+f.open(nama_file);
+f.close();
+
+print "[=======================================================]\n";    
+ofstream asw(nama_file);
+if (asw.is_open()){
+    teks data;
+    do{
+        getline(cin,data);
+        cout << "- ";
+        asw << data << endl;
+    }while(data != "'");
+    asw.close();
+}
+return 0;
+print "[=======================================================]\n";
+}
